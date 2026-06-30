bd85c1f01b68495b0469ffbfffc455c68fa84d5e
ivan-kardoso <ikardoso.mobile@gmail.com>
2026-06-30 11:25:36 -0300

    feat(config): adiciona carregamento de configuração geral via TOML

--- binance/usdm_futures/infra/config/__init__.py
+++ binance/usdm_futures/infra/config/__init__.py
@@ -1,3 +1,19 @@
 from .secrets import Secrets
+from .system_settings import (
+    FetchConfig,
+    MonitoringConfig,
+    ExecutionConfig,
+    LoggingConfig,
+    SystemSettings,
+    load_system_settings,
+)
 
-__all__ = ["Secrets"]
+__all__ = [
+    "Secrets",
+    "FetchConfig",
+    "MonitoringConfig",
+    "ExecutionConfig",
+    "LoggingConfig",
+    "SystemSettings",
+    "load_system_settings",
+]
