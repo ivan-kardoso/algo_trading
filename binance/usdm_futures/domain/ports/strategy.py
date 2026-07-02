d9a2d47e1a28bd73a9077f7319e82d85ad868e05
ivan-kardoso <ikardoso.mobile@gmail.com>
2026-07-02 09:49:04 -0300

    ajuste no módulo strategy.py

--- binance/usdm_futures/domain/ports/strategy.py
+++ binance/usdm_futures/domain/ports/strategy.py
@@ -4,11 +4,12 @@ from abc import ABC, abstractmethod
 from typing import Literal
 
 from . import OHLCVData
+from ..models.indicator_data import IndicatorData
 
 
 class IStrategyPort(ABC):
     @abstractmethod
-    def apply_indicators(self, data: OHLCVData) -> OHLCVData: ...
+    def apply_indicators(self, data: OHLCVData) -> IndicatorData: ...
 
     @abstractmethod
-    def check_signal(self, data: OHLCVData) -> Literal["buy", "sell"] | None: ...
+    def check_signal(self, data: IndicatorData) -> Literal["buy", "sell"] | None: ...
